8d888827fa5cda2c0f811eadebdfb5a8dca06878
viettv1 <viettv1@smartosc.com>
2025-12-31 16:12:22 +0700

    aaa

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <PrismicPreview repositoryName={repositoryName} />
+        {/* <PrismicPreview repositoryName={repositoryName} /> */}
       </body>
     </html>
   );
